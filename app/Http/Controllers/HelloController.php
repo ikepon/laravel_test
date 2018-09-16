fb780442231573c81b7e00abb51b76903e768845
ikepon <tatsuyanoheya@gmail.com>
2018-09-16 16:13:35 +0900

    Add route param

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index() {
-      $data = ['msg' => 'これはコントローラーから渡されたメッセージです。'];
+    public function index($id='zero') {
+      $data = [
+        'msg' => 'これはコントローラーから渡されたメッセージです。',
+        'id' => $id
+      ];
       return view('hello.index', $data);
     }
 }
