4b1b3aa99772a9a754cac075ffb255cb59c92efd
ikepon <tatsuyanoheya@gmail.com>
2018-09-17 18:48:08 +0900

    Use Sqlite

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -4,6 +4,8 @@
   <style>
     body { font-size: 16pt; color: #999; }
     h1 { font-size: 100pt; text-align:right; color: #eee; margin: -40px 0 -50px 0; }
+    th{background-color: #999; color: fff; padding: 5px 10px;}
+    td{border: solid 1px #aaa; color: #999; padding: 5px 10px;}
   </style>
 </head>
 <body>
@@ -18,5 +20,15 @@
     <input type="text" name="msg">
     <input type="submit">
   </form>
+  <table>
+    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+    @foreach ($items as $item)
+      <tr>
+        <td>{{$item->name}}</td>
+        <td>{{$item->mail}}</td>
+        <td>{{$item->age}}</td>
+      </tr>
+    @endforeach
+  </table>
 </body>
 </html>
